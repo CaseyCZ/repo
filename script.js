ee6d68b7798e58aeba61e7517bd36f8f1a96a7bf
CaseyCZ <kyselalukas@seznam.cz>
2023-08-25 12:21:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 <script>
- const nightModeButton = document.getElementById('nightModeButton');
-  const body = document.body;
-
-  nightModeButton.addEventListener('click', () => {
-    body.classList.toggle('night-mode');
-  });
 const nightModeButton = document.getElementById('nightModeButton');
 const body = document.body;
 const sunMoonIcon = document.getElementById('sunMoonIcon');
 
 nightModeButton.addEventListener('click', () => {
   body.classList.toggle('night-mode');
-  sunMoonIcon.classList.toggle('moon');
+  sunMoonIcon.classList.toggle('fa-sun'); // Přepínání ikony slunce/měsíce
+  sunMoonIcon.classList.toggle('fa-moon');
 });
 
   </script>
